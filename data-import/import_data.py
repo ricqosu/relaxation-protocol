c338765e42abf3014265f811d856ca43fee22b6e
ricqosu <quachr@oregonstate.edu>
2023-11-19 20:02:23 -0800

    Reorder table drop order for foreign key restraint

--- data-import/import_data.py
+++ data-import/import_data.py
@@ -10,35 +10,36 @@ connection_string = os.getenv('DATABASE_URL')
 connection = psycopg2.connect(connection_string)
 cursor = connection.cursor()
 
-# Create days table
+cursor.execute("DROP TABLE IF EXISTS tasks")
 cursor.execute("DROP TABLE IF EXISTS days")
+
+# Create days table
 cursor.execute("""
     CREATE TABLE days(
         id SERIAL PRIMARY KEY, 
         day_number SMALLINT)
     """)
 
-# Insert day data
-for i in range(1, 16):
-    cursor.execute("INSERT INTO days(day_number) VALUES(%s)", (i,))
-
 # Create tasks table
-cursor.execute("DROP TABLE IF EXISTS tasks")
 cursor.execute("""
     CREATE TABLE tasks(
         id SERIAL PRIMARY KEY,
-        day_id INTEGER REFERENCES days(id)
+        day_id INTEGER REFERENCES days(id),
         task TEXT,
         duration SMALLINT
     )
 """)
 
+# Insert day data
+for i in range(1, 16):
+    cursor.execute("INSERT INTO days(day_number) VALUES(%s)", (i,))
+
 # Read data from CSV and populate tasks table
 current_directory = os.path.dirname(os.path.abspath(__file__))
-for i in range(1, 2, 1):
+for i in range(1, 3, 1):
     current_file = os.path.join(current_directory, 'data', f'day_{i}.csv')
     data_frame = pandas.read_csv(current_file)
-    tasks = data_frame['Tasks']
+    tasks = data_frame['Task']
     durations = data_frame['Duration']
     tasks_durations_zipped = list(zip(tasks, durations))
     for task, duration in tasks_durations_zipped:
